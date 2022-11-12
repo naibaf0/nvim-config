9ce0c07287d213ec01a1cb74961a4e3b3cbf5d26
Fabian Hinz <hallo@naibafo.de>
2022-11-12 18:11:08 +0100

    Update deprecated option

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -115,7 +115,7 @@ function M.setup()
     --
     -- Add additional capabilities supported by nvim-cmp
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
     local lspconfig = require('lspconfig')
 
