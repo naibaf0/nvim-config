cad9bc8e3c0398f50db0d8c436004870e9688d94
Fabian Hinz <hallo@naibafo.de>
2023-07-19 01:47:11 +0200

    Set correct bg

--- lua/plugin-conf.lua
+++ lua/plugin-conf.lua
@@ -4,7 +4,7 @@ function M.setup()
 
     ----- Catppuccin Colorscheme {{{------------------------------------------------------------------------------------
     require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        flavour = "macchiato", -- latte, frappe, macchiato, mocha
         transparent_background = false, -- disables setting the background color.
         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
         term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
