fa28c598f01b288ac231617715f00e5593c7adad
Fabian Hinz <hallo@naibafo.de>
2023-09-24 14:40:44 +0200

    disable vimtex compile quickfix window with errors

--- lua/plugin-conf.lua
+++ lua/plugin-conf.lua
@@ -337,6 +337,7 @@ function M.setup()
     ----- VimTeX {{{----------------------------------------------------------------------------------------------------
     vim.g.tex_flavor = 'latex'
     vim.g.vimtex_compiler_method = 'latexmk'
+    vim.g.vimtex_quickfix_mode = 0
     vim.g.vimtex_view_method = 'zathura'
     vim.g.vimtex_view_zathura_options = '-x "nvr --servername ' .. vim.api.nvim_get_vvar('servername') .. ' --remote-silent %{input} -c %{line}"'
     vim.g.tex_conceal='abdmg'
